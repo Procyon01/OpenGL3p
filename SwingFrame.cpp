f2e0650a92b3043f59790dda17bbd1b2317b725a
Charles <fallertc@mail.gvsu.edu>
2015-03-14 18:53:06 -0400

    swingframe
    woo

--- SwingFrame.cpp
+++ SwingFrame.cpp
@@ -6,14 +6,14 @@ void SwingFrame::build() {
 }
 
 void SwingFrame::render() const {
-    static float COPPER_AMBIENT[] = {0.191250, 0.073500, 0.022500, 1.000000};
-    static float COPPER_DIFFUSE[] = {0.703800, 0.270480, 0.082800, 1.000000};
-    static float COPPER_SPECULAR[] = {0.256777, 0.137622, 0.086014, 1.000000};
+    static float COPPER_AMBIENT[] = {0.250000, 0.250000, 0.250000, 1.000000};
+    static float COPPER_DIFFUSE[] = {0.400000, 0.400000, 0.400000, 1.000000};
+    static float COPPER_SPECULAR[] = {0.774597, 0.774597, 0.774597, 1.000000};
 
     glMaterialfv(GL_FRONT, GL_AMBIENT, COPPER_AMBIENT);
     glMaterialfv(GL_FRONT, GL_DIFFUSE, COPPER_DIFFUSE);
     glMaterialfv(GL_FRONT, GL_SPECULAR, COPPER_SPECULAR);
-    glMaterialf(GL_FRONT, GL_SHININESS, 12.8);
+    glMaterialf(GL_FRONT, GL_SHININESS, 76.800003);
     glPushMatrix();
     glRotatef (90, 1, 0, 0);
     glScalef (1.75, 1.75, 10);
